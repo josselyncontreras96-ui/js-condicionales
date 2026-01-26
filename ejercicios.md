e806105117e11cbcb0f27ad6a4c5a8507b1cacf2
Josselyn Contreras <josselyn.contreras96@gmail.com>
2026-01-26 21:54:12 +0100

    js-condiciones y estructura terminada

---
+++ ejercicios.md
@@ -0,0 +1,2 @@
+# Ejercicio clase 02
+
---
+++ scripts.js
@@ -0,0 +1,11 @@
+let edad = 15;
+
+if (edad < 13) {
+  console.log("Niño");
+} else if (edad >= 13 && edad <= 17) {
+  console.log("Adolescente");
+} else {
+  console.log("Adulto");
+}
+
+
