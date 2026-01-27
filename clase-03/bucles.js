4f08a6990b1905e69493f2e9931dc57c1e646d18
Josselyn Contreras <josselyn.contreras96@gmail.com>
2026-01-27 22:00:09 +0100

    agrego distintos ciclos y bucles

---
+++ clase-03/bucles.js
@@ -0,0 +1,54 @@
+// let i = 3;
+
+//while (i <= 0) {
+  //console.log(i);
+  //i--;
+//}
+ 
+
+//let palabra = prompt("Ingrese la palabra Hola");
+
+//while (palabra !== "Hola") {
+ // console.log("Ingresaste:", palabra);
+ // palabra = prompt("Error. Debes ingresar 'Hola'");
+//}
+
+//console.log("Bien! Fin del programa");
+
+//let i = 1;
+
+//while (i <= 5) {
+  //console.log(i);
+  //i++;
+//}
+
+//let palabra = prompt("Ingrese la palabra JS");
+
+//while (palabra !== "JS") {
+  //palabra = prompt("Incorrecto. Ingrese la palabra JS");
+//}
+
+//console.log("Correcto! Ingresaste JS");
+
+//let numero;
+
+//do {
+ // numero = prompt("Ingrese el número 5");
+ // console.log("Ingresaste:", numero);
+//} while (numero !== "5");
+
+//console.log("Correcto, ingresaste el número 5");
+
+//for (let i = 1; i <= 5; i++) {
+  //console.log(i);
+//}
+
+for (let i = 1; i <= 7; i++) {
+  if (i !== 5) {
+    console.log(i);
+  }
+}
+ 
+
+
+
--- ejercicios.md
+++ ejercicios.md
@@ -1,2 +1,4 @@
-# Ejercicio clase 02
+# Ejercicio clase 0203
+1. crear un while que muestre los numeros del 1 al 5
+2 usar prompt para pedir una palabra hasta que sea JS
 
