58c3b946ee4468696df47e254d0ff7ea632071a7
Josselyn Contreras <josselyn.contreras96@gmail.com>
2026-01-30 15:48:26 +0100

    añado mas ejemplos

---
+++ clase-05/js/scripts.js
@@ -0,0 +1,15 @@
+const nombres = ["Juan"," Sofia", "Andrea", "Ana"];
+
+//console.log(nombres[2]);
+
+/*nombres[2 ] = "Jose";
+console.log(nombres);
+console.log(nombres[nombres.length - 1]);
+nombres.push("Angelo", "Carlos");
+*/
+console.log(nombres);
+
+for (let i = 0; i < nombres.length; i++) {
+    console.log(i, nombres[i])
+    
+}
