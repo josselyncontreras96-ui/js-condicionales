6bfbd817ba2e0e8147b48d7083c18d6da6cdb8d9
Josselyn Contreras <josselyn.contreras96@gmail.com>
2026-02-03 19:03:00 +0100

    agrego mas ejemplo

---
+++ clase-07/js/scripts.js
@@ -0,0 +1,11 @@
+/*const titulo = document.getElementById("subtitulo");
+
+console.log(titulo.textContent);
+
+titulo.textContent = "Subtitulo de coches";*/
+
+const textos = document.getElementsByClassName("texto")
+
+console.log(textos);
+console.log(textos.length);
+console.log(textos[1]) 
