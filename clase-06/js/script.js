22283e91b60963067e4fc08418d83a58769fefc4
Josselyn Contreras <josselyn.contreras96@gmail.com>
2026-02-03 14:37:28 +0100

    guardo ultimos cambios

---
+++ clase-06/js/script.js
@@ -0,0 +1,47 @@
+//const usuario = {
+   // nombre: "Juan",
+  //  apellido: "Perez",
+  //  edad: 30,
+  //  perimisos: ["admin", "editor"],
+//}
+//console.log(usuario.nombre);
+
+//usuario.nombre = "Juan Perez";
+
+//console.log(usuario);
+
+//const alumno = {
+ // nombre: "Cristian Alvarez",
+   // edad: 30
+ // };
+ // console.log(alumno.nombre);
+ // alumno.edad = 31;
+ // console.log(alumno.edad);
+ // const producto = {
+  //  nombre: "movil",
+  //  precio: 1500
+ // };
+ // console.log(producto.precio);
+ // producto.precio = 1800;
+ // console.log(producto.precio);
+
+/*const producto = {
+  nombre: "movil",
+  precio: 1500
+};
+console.log(producto["precio"]);
+const propiedad = "nombre";
+console.log(producto[propiedad]);
+
+// agregar propiedades con notacion de punto y corccetes
+usuario.edad = 38;
+usuario["edad"] = 38;
+
+usuario.ciudad = "Caracas";
+usuario["pais"] = "Venezuela";
+*/
+
+const usuario = {
+
+    
+}
